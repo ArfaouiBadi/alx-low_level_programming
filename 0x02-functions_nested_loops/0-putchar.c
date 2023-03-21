eac39a77de9e79559bca668c3d371207ab68a085
root <Arfaouibadi19@gmail.com>
2023-03-21 08:26:56 -0700

    k

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,6 +2,14 @@
 
 int main(void)
 {
-    _putchar('\n');
-    return 0;
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n'); /* Add a newline character */
+    return (0);
 }
