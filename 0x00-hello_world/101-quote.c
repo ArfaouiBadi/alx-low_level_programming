42ffc0e3db4d3dd9ae186cd683a1d861a12cf829
root <Arfaouibadi19@gmail.com>
2023-03-20 18:24:06 -0700

    j

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+  char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  int len = 0;
+
+  while (str[len])
+    len++;
+
+  write(2, str, len);
+
+  return (1);
+}
