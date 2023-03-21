7790c2e98f564b354fa437518b98de092960b3c1
root <Arfaouibadi19@gmail.com>
2023-03-21 14:25:51 -0700

    g

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,13 +2,11 @@
 
 int print_sign(int n) {
     if (n > 0) {
-        printf("+%d", n);
-        return 1;
+      
+        return "+1";
     } else if (n < 0) {
-        printf("-%d", -n);
-        return -1;
+        return "-1";
     } else {
-        printf("0");
-        return 0;
+        return "00";
     }
 }
