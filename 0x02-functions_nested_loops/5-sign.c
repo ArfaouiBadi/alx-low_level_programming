353f13959744b925da37ef86b2e120d46542e273
root <Arfaouibadi19@gmail.com>
2023-03-21 14:23:04 -0700

    l

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
-/**Main fucntion
- *
- **/
 
 int print_sign(int n) {
-if (n > 0) {
-printf("+");
-return 1;
-}
-else if (n < 0) {
-printf("-");
-return -1;
-}
-else {
-printf("0");
-return 0;
+    if (n > 0) {
+        printf("+%d", n);
+        return 1;
+    } else if (n < 0) {
+        printf("-%d", -n);
+        return -1;
+    } else {
+        printf("0");
+        return 0;
     }
-}
+}}
