68d0956b6cd182570fda31d0f25cb619b597fcea
root <Arfaouibadi19@gmail.com>
2023-03-21 17:28:01 -0700

    g

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,20 +1,88 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    unsigned int len;
+    unsigned int cap;
+    unsigned char *data;
+} bigint;
+
+void bigint_init(bigint *x) {
+    x->len = 0;
+    x->cap = 16;
+    x->data = malloc(x->cap);
+    memset(x->data, 0, x->cap);
+}
+
+void bigint_deinit(bigint *x) {
+    free(x->data);
+}
+
+void bigint_copy(bigint *dst, bigint *src) {
+    if (dst->cap < src->len) {
+        dst->cap = src->len;
+        dst->data = realloc(dst->data, dst->cap);
+    }
+    dst->len = src->len;
+    memcpy(dst->data, src->data, src->len);
+}
+
+void bigint_add(bigint *x, bigint *y) {
+    unsigned int i;
+    unsigned char carry = 0;
+    unsigned char *p1 = x->data;
+    unsigned char *p2 = y->data;
+    unsigned char *p3 = x->data;
+    for (i = 0; i < x->len || i < y->len; ++i) {
+        unsigned char a = i < x->len ? *p1++ : 0;
+        unsigned char b = i < y->len ? *p2++ : 0;
+        unsigned char c = a + b + carry;
+        carry = c >> 8;
+        *p3++ = c & 0xff;
+    }
+    if (carry) {
+        *p3++ = carry;
+        ++i;
+    }
+    x->len = i;
+}
+
+void bigint_print(bigint *x) {
+    int i;
+    printf("%d", x->data[x->len - 1]);
+    for (i = x->len - 2; i >= 0; --i) {
+        printf(", %03d", x->data[i]);
+    }
+    printf("\n");
+}
 
 int main() {
-    int sum = 0;
-    int fib1 = 1;
-    int fib2 = 2;
+    bigint x, y, z;
+    bigint_init(&x);
+    bigint_init(&y);
+    bigint_init(&z);
 
-    while (fib2 <= 4000000) {
-        if (fib2 % 2 == 0) {
-            sum += fib2;
-        }
-        int tmp = fib2;
-        fib2 += fib1;
-        fib1 = tmp;
+    x.data[0] = 1;
+    x.len = 1;
+
+    y.data[0] = 2;
+    y.len = 1;
+
+    bigint_print(&x);
+    bigint_print(&y);
+
+    unsigned int i;
+    for (i = 2; i < 98; ++i) {
+        bigint_add(&z, &x);
+        bigint_copy(&x, &y);
+        bigint_copy(&y, &z);
+        bigint_print(&z);
     }
 
-    printf("%d\n", sum);
+    bigint_deinit(&x);
+    bigint_deinit(&y);
+    bigint_deinit(&z);
 
     return 0;
 }
