c70be661564e3eca2fc2f1e86a7e15a6aea20804
root <Arfaouibadi19@gmail.com>
2023-03-21 17:15:40 -0700

    natual'

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,15 +1,25 @@
 #include "main.h"
-
-int sum_multiples_3_5(int n) {
+/**
+ * sum_multiples_3_5 - Computes the sum of all multiples of 3 or 5 below n
+ * @n: The upper limit of the range to check for multiples
+ *
+ * Return: The sum of all multiples of 3 or 5 below n
+ */
+int sum_multiples_3_5(int n)
+{
 int sum = 0;
 int i;
-for (i = 0; i < n; i++) {
-if (i % 3 == 0 || i % 5 == 0) {
+for (i = 0; i < n; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
 sum += i;
 }
 }
-return sum;}
-int main(void) {
+return sum;
+}
+int main(void)
+{
 printf("%d\n", sum_multiples_3_5(1024));
 return 0;
 }
