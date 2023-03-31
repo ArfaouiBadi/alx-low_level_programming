492d9fa7d1c852d44251f26c45089e3960544eaf
root <Arfaouibadi19@gmail.com>
2023-03-31 12:00:11 -0700

    ad

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,39 @@
+#include <main.h>
+/**
+ * rot13 - Encode a string using ROT13 cipher
+ *
+ * @s: the string to encode
+ *
+ * Return: a pointer to the encoded string
+ */
+char *rot13(char *s)
+{
+    int i, j;
+    char *res = s;
+
+    for (i = 0; s[i]; i++)
+    {
+        if ((s[i] >= 'A' && s[i] <= 'M') || (s[i] >= 'a' && s[i] <= 'm'))
+        {
+            for (j = 0; j < 13; j++)
+            {
+                res[i]++;
+                if (res[i] == 'n')
+                    break;
+            }
+        }
+        else if ((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
+        {
+            for (j = 0; j < 13; j++)
+            {
+                res[i]++;
+                if (res[i] == ('z' + 1))
+                    res[i] = 'a';
+                else if (res[i] == ('Z' + 1))
+                    res[i] = 'A';
+            }
+        }
+    }
+
+    return res;
+}
